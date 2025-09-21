fac382b63bfc57ff5f44f5d799728e23d2e32d9a
Suhaitsht <nm.suhaitsuhait@gmail.com>
2025-09-21 13:51:34 +0530

    modifed

--- utils/data.js
+++ utils/data.js
@@ -79,26 +79,26 @@ export const testimonials = [
 ];
 
 // // footer section
-// const footerLinks = {
-//   company: [
-//     { name: "About", href: "#" },
-//     { name: "Terms of Use", href: "#" },
-//     { name: "Privacy Policy", href: "#" },
-//     { name: "How it Works", href: "#" },
-//     { name: "Contact Us", href: "#" },
-//   ],
-//   getHelp: [
-//     { name: "Support Carrer", href: "#" },
-//     { name: "24h Service", href: "#" },
-//     { name: "Quick Chat", href: "#" },
-//   ],
-//   support: [
-//     { name: "FAQ", href: "#" },
-//     { name: "Policy", href: "#" },
-//     { name: "Business", href: "#" },
-//   ],
-//   contact: [
-//     { name: "WhatsApp", href: "#" },
-//     { name: "Support 24", href: "#" },
-//   ],
-// };
+export const footerLinks = {
+  company: [
+    { name: "About", href: "#" },
+    { name: "Terms of Use", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "How it Works", href: "#" },
+    { name: "Contact Us", href: "#" },
+  ],
+  getHelp: [
+    { name: "Support Carrer", href: "#" },
+    { name: "24h Service", href: "#" },
+    { name: "Quick Chat", href: "#" },
+  ],
+  support: [
+    { name: "FAQ", href: "#" },
+    { name: "Policy", href: "#" },
+    { name: "Business", href: "#" },
+  ],
+  contact: [
+    { name: "WhatsApp", href: "#" },
+    { name: "Support 24", href: "#" },
+  ],
+};
